7a491bdb9b9db18b82115df46cd7a49723658705
sochan-dev <halosakakitajima@gmail.com>
2021-05-11 18:12:40 +0900

    sassの自動コンパイルと、テスト用のcssファイルの自動生成。 Eslintの設定ファイルの監視除外。

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import Styles from 'styles/sass/test.module.scss';
 
 const Test: React.FC = () => {
   type f = {
     a: String;
   };
 
-  const b: f = {
+  const s: f = {
     a: 'ff',
   };
 
   console.log('testLog');
-  return <div>Test!!!!!!</div>;
+  return (
+    <div className={Styles.test}>
+      <p>success!!!!!!</p>
+    </div>
+  );
 };
 export default Test;
